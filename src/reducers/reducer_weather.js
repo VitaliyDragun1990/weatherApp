d073f38519124c0dc60646a55a9c349b72025829
Vitaly Dragun <vdrag00n90@gmail.com>
2018-02-13 18:06:03 +0200

    Add some uncommited files

---
+++ src/reducers/reducer_weather.js
@@ -0,0 +1,10 @@
+import { FETCH_WEATHER } from "../actions";
+
+export default function (state = [], action) {
+    switch (action.type) {
+        case FETCH_WEATHER:
+            // this create new array containing new data and all data from old state
+            return [action.payload.data, ...state];
+    }
+    return state;
+}
